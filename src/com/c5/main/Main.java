6eccd7a86f8892362b98b0071a7b978182d71d4f
blackDs <rassemzitouni@gmail.com>
2019-02-26 17:45:51 +0100

    11

---
+++ src/com/c5/main/Main.java
@@ -0,0 +1,14 @@
+package com.c5.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("**************menu**************");
+		System.out.println("1-ajouter un article");
+		System.out.println("2-modifieer un article");
+		System.out.println("3-supprimmer un article");
+		System.out.println("4-recherch un artcicle");
+		System.out.println("********************************");
+	}
+
+}
