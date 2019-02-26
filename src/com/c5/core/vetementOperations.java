8440901f65532cca9a316bb63e431f3ceb81b355
akramEsprit <akram.memni@esprit.tn>
2019-02-26 15:16:51 +0100

    les enums

---
+++ src/com/c5/core/vetementOperations.java
@@ -0,0 +1,11 @@
+package com.c5.core;
+
+public interface vetementOperations {
+	
+	public void ajouterProduit(Vetement vetement);
+	public void modifierProduit();
+	public void supprimerProduit(Vetement vetement);
+	public void ajouterPromotion(int promotion);
+	
+
+}
---
+++ src/com/c5/enumeration/emballage.java
@@ -0,0 +1,5 @@
+package com.c5.enumeration;
+
+public enum emballage {
+
+}
