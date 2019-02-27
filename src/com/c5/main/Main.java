6546bb27221d5fa15a7318db353d80516a9cb3dd
blackDs <rassemzitouni@gmail.com>
2019-02-27 14:27:33 +0100

    aa

--- src/com/c5/main/Main.java
+++ src/com/c5/main/Main.java
@@ -1,14 +1,92 @@
 package com.c5.main;
 
-public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("**************menu**************");
-		System.out.println("1-ajouter un article");
-		System.out.println("2-modifieer un article");
-		System.out.println("3-supprimmer un article");
-		System.out.println("4-recherch un artcicle");
-		System.out.println("********************************");
-	}
-
+ 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+ 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+ 
+/**
+ * This program demonstrates how to use JPanel in Swing.
+ * @author www.codejava.net
+ */
+public class Main extends JFrame {
+     
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JLabel labelUsername = new JLabel("Enter username: ");
+    private JLabel labelPassword = new JLabel("Enter password: ");
+    private JTextField textUsername = new JTextField(20);
+    private JPasswordField fieldPassword = new JPasswordField(20);
+    private JButton buttonLogin = new JButton("Login");
+     
+    public void SwingJPanelDemo() {
+       
+         
+        // create a new panel with GridBagLayout manager
+        JPanel newPanel = new JPanel(new GridBagLayout());
+         
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10, 10, 10, 10);
+         
+        // add components to the panel
+        constraints.gridx = 0;
+        constraints.gridy = 0;     
+        newPanel.add(labelUsername, constraints);
+ 
+        constraints.gridx = 1;
+        newPanel.add(textUsername, constraints);
+         
+        constraints.gridx = 0;
+        constraints.gridy = 1;     
+        newPanel.add(labelPassword, constraints);
+         
+        constraints.gridx = 1;
+        newPanel.add(fieldPassword, constraints);
+         
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        newPanel.add(buttonLogin, constraints);
+         
+        // set border for the panel
+        newPanel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Login Panel"));
+         
+        // add the panel to this frame
+        add(newPanel);
+         
+        pack();
+        setLocationRelativeTo(null);
+    }
+     
+    public static void main(String[] args) {
+        // set look and feel to the system look and feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+         
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                
+            }
+        });
+    }
 }
